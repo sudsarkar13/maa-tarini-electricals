2a04aa6d3cfefeba7babaedd846271c81da6953e
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-19 00:53:38 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import Head from "next/head";
 import Hero from "@/components/Hero";
 
 const HomePage = () => {
 	return (
+    <Head>
+      <title>Maa Tarini Electricals</title>
+    </Head>
 		<div>
 			HomePage
 			{/* Hero Page */}
