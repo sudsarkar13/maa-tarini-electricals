d7b962206a9e0a31b07b27e19f846ca9a2425a0f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-19 19:19:14 +0530

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<div className='h-full bg-[#a03000] w-full'>
+			<div className="h-[90vh] w-[100%] bg-[url('/images/Background.png')] bg-cover bg-center mt-[20vh] "></div>
+		</div>
+	);
 };
 
 export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 import Hero from "@/components/Hero";
 
 const HomePage = () => {
@@ -9,8 +10,7 @@ const HomePage = () => {
 			<Head>
 				<title>Maa Tarini Electricals</title>
 			</Head>
-			<div>
-				HomePage
+			<div className='background'>
 				{/* Hero Page */}
 				<Hero />
 			</div>
